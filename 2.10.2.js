429c1e503084f2e8d577463443a360075ca83a39
Grekos89 <70657407+Grekos89@users.noreply.github.com>
2020-09-07 17:02:14 +0300

    Create 2.10.2.js

---
+++ 2.10.2.js
@@ -0,0 +1,9 @@
+<script>
+    'use strict'
+    let name = prompt('Какое "офицеальное" название JavaScript?', '');
+    if (name == 'ECMAScript') {
+    alert ("Верно!");    
+    } else {
+    alert ("Не знаете? ECMAScript!");    
+    }
+</script>
